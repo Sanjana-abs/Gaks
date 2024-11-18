e193e8ce73e7be2502ec242e22e608044aee83ce
Sanjana-abs <sanjanasnj786@gmail.com>
2024-11-18 13:47:12 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import About from './View/About'
+import Contact from './View/Contact'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import { BrowserRouter } from "react-router-dom";
+
 
 // import Meals from './View/Meals'
 // import MealsPlans from './View/MealsPlans'
 
 export default function App() {
   return (
-    <div>
-     
+   
+     <Router>
+      <Routes>
       {/* <Meals></Meals>
       <MealsPlans></MealsPlans> */}
-      <About></About>
-    </div>
+      {/* <About></About>
+      <Contact></Contact> */}
+      <Route Path="/" element={<About/>}/>
+      <Route Path="/Contact" element={<Contact/>}/>
+    
+    </Routes>
+    </Router>
   )
 }
--- src/View/Contact.js
+++ src/View/Contact.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import "../style/Contact.css";
 
-export default function Contact() {
+
+function Contact() {
   return (
-    <div>
-        
+    <div className='contact'>
+      <h1>Contact Us</h1>
+      <p><a href="mailto:info@mysite.com">info@mysite.com</a></p>
+      <p>Tel: 123-456-7890</p>
+      <p>
+        500 Terry Francine Street,<br />
+        San Francisco, CA 94158
+      </p>
+
+      <form className="subscribe-form">
+        <label htmlFor="email">Keep Updated on Our Latest Offers</label><br />
+        <input 
+          type="email" 
+          id="email" 
+          name="email" 
+          placeholder="Enter your email" 
+          required 
+        />
+        <div className="checkbox-container">
+          <input 
+            type="checkbox" 
+            id="newsletter" 
+            name="newsletter" 
+          />
+          <label htmlFor="newsletter">Yes, subscribe me to your newsletter.</label>
+        </div>
+        <button type="submit">Subscribe</button>
+      </form>
     </div>
-  )
+  );
 }
+ export default Contact;
